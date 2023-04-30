a18c272a7969abc385c085eba2fb4eba0656988b
Hebrews Gebremariam <83418575+hebree123@users.noreply.github.com>
2023-04-30 16:37:46 -0400

    Resolve accessibility issues

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Barstool Fullstack Challenge</title>
       <Head />
       <body>
-        <Main />
+        <Main role="main" />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
